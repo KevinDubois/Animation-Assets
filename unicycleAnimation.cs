29a82b845f4466a308d6d38f28a4675f45adad01
KevinDubois <kd1277@nyu.edu>
2015-04-28 15:12:00 -0400

    Animation Assets

---
+++ unicycleAnimation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class unicycleAnimation : MonoBehaviour {
+
+	public float speed;
+	public Animator animator;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		animator.SetFloat("speed", speed);
+	
+	}
+}
